95671896fe28b9657c95c1c6bfb8cee7f9c626fb
MHD <150580476+mahdimazhabi@users.noreply.github.com>
2025-12-19 20:33:49 +0330

    Add files via upload

---
+++ project/helper.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "helper.h"
+
+void say_hello(void) {
+    printf("Hello from helper function!\n");
+}
+
---
+++ project/helper.h
@@ -0,0 +1,7 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+void say_hello(void);
+
+#endif
+
